fcdcdeace8b45564d29794a291b65b4eea06fca8
Gerard Eklu <gerard_eklu@homedepot.com>
2019-12-28 01:25:59 -0500

    Dispatching actions

--- redux-stuff.js
+++ redux-stuff.js
@@ -8,10 +8,26 @@ const initialState = {
 
 // Reducer 
 const rootReducer = (state = initialState, action) => {
+    if (action.type === "INCREMENT_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+    if (action.type === "ADD_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
     return state;
 }
 
 // Store
 const store = createStore(rootReducer);
+console.log(store.getState());
 
+// Dispatch Actions
+store.dispatch({ type: "INCREMENT_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
 console.log(store.getState());
