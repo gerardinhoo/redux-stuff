bb4215456cf1255bd84bb996f27cbe6ef8aa8e5a
Gerard Eklu <gerard_eklu@homedepot.com>
2019-12-28 14:24:31 -0500

    Dispatched Actions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux"
 import { createStore } from "redux";
 import './index.css';
 import App from './App';
@@ -8,5 +9,5 @@ import reducer from "./store/reducer";
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
