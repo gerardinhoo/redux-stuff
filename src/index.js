c7d281a5e06e5de51606d08bdd2c187d21c402d3
Gerard Eklu <gerard_eklu@homedepot.com>
2020-01-12 00:07:25 -0500

    WIP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux"
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -14,8 +14,20 @@ const rootReducer = combineReducers({
 })
 
 
+const logger = (store) => {
+    return next => {
+        return action => {
+            console.log("Middleware dispatched", action);
+            const result = next(action);
+            console.log("middleware next state", store.getState());
+            return result;
+        }
+    }
+}
 
-const store = createStore(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
+
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
